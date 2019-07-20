d52c8194c9fa8a432c64e63aca703e4041494b64
John Salatas <jsalatas@gmail.com>
2019-07-20 16:53:48 -0700

    RegTestParams
    fixed genesis hash

--- core/src/main/java/org/bitcoinj/params/RegTestParams.java
+++ core/src/main/java/org/bitcoinj/params/RegTestParams.java
@@ -39,7 +39,7 @@ public class RegTestParams extends AbstractBitcoinNetParams {
         genesisBlock.setTime(1296688602L);
         spendableCoinbaseDepth = 100;
         String genesisHash = genesisBlock.getHashAsString();
-        checkState(genesisHash.equals("00000007199508e34a9ff81e6ec0c477a4cccff2a4767a8eee39c11db367b008"));
+        checkState(genesisHash.equals("b46086f09926d7881f8eb7ad62a9e3d1a3d98d957ac6b00ca673b652aaff40b8"));
         dnsSeeds = null;
         addrSeeds = null;
         bip32HeaderP2PKHpub = 0x043587cf; // The 4 byte header that serializes in base58 to "tpub".
